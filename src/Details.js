2d7e26062b55c86052dece104a35446ef31c23cd
Prachi-556 <Prachi-556prachivishwakarma06@gmail.com>
2024-10-03 22:01:15 +0530

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,172 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Prachi Vishwakarma",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Final year computer science student pursuing BCA from Sant Hirdaram Girls College. I have keen interest 
+  in web technologies. Apart from acadamic pursuits I like travelling and reading books. I am passionate and hardworking.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+
+  github: "https://github.com/Prachi-556",
+ 
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Web Technologies",
+    Company: `Techdev`,
+    Location: "Bhopal",
+    Type: "Certificate",
+    Duration: "Aug 2024 - Sep 2024",
+  },
+  {
+    Position: "Advance Python",
+    Company: `Techdev`,
+    Location: "Bhopal",
+    Type: "Certificate",
+    Duration: "July 2023 - Aug 2023",
+  },
+  {
+    Position: " Basics of Python",
+    Company: `Techdev`,
+    Location: "Bhopal",
+    Type: "Certificate",
+    Duration: "dec 2022 - jan 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor's of Computer Application",
+    Company: "Sant Hirdaram Girls College",
+    Location: "Bhopal",
+    Type: "Full Time",
+    Duration: "June 2022 - Present",
+  },
+  {
+    Position: "Senior Secondary",
+    Company: `Red Rose School`,
+    Location: "Bhopal",
+    Type: "Full Time",
+    Duration: "Aug 2021 - jun 2022",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  
+  bootstrap: bootstrap,
+  vscode: vscode,
+  
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Blog Project",
+    image: projectImage1,
+    description: `Blog Project allows to create , read , update and delete blogs.You can create custom blogs here`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "prachivishwakarma06.com",
+  phone: "+91 12345 67890",
+};
